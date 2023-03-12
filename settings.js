da3d65a7c87790988cb4a42a140f9ea78c13a1da
NexenFull14 <100863416+NexenFull14@users.noreply.github.com>
2023-03-12 13:52:19 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
+
+if (document.getElementById("check1").checked)
+{
+        document.body.classList.toggle("dark-theme");
+}
+if (document.getElementById("check1").checked == false){
+        document.body.classList.remove("dark-theme");}
